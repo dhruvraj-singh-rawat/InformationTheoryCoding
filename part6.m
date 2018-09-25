3a84dddb720f80ece3aa2993fc1d7f6c72a0d4e8
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-09-25 22:21:48 +0530

    Modified: Part 6

--- part6.m
+++ part6.m
@@ -2,19 +2,18 @@ clc;
 clear all;
 close all;
 
-input_binary=[ 1 0 0 0 1 1 0 ];
+input_binary=[ 1 0 1 0 1 0 1 ];
 
-a = 1; % Lower Limit
-b = 5; % Higher Limit
-random_noise = floor((b-a).*rand(length(input_binary),1) + a);
+repeat_times=3; % Change this Variable to change how many times variable must Repeat
 
 input_coded=[];
 pos=1;
-
 for i=1:length(input_binary)
-    for j=1:random_noise(i)
-        input_coded(pos)=input_binary(i);
-        pos=pos+1;
-    end
+    count=1;
+ while(count <= repeat_times )
+     input_coded(pos)=input_binary(i);
+     count=count+1;
+     pos=pos+1;
+ end
     
 end
